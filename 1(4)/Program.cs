fa1906930241c432bbb60ddf3facff2fb8f8ca37
korshun1997 <116491146+korshun1997@users.noreply.github.com>
2022-11-14 22:46:52 +0300

    add files

---
+++ 1(4)/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("enter number a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter number b");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter number c");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a<b)
+{
+    max = b;
+}
+if (c> max)
+{
+    max = c;
+}
+Console.WriteLine($"Максимум из чисел равен {max}");
---
+++ 1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 2. Даны 2 числа а и в. Если а больше, чем в - вывести соответствующую надпись, если нет - наоборот.  
+Console.WriteLine("enter number a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter number b");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    Console.WriteLine("a больше, чем b");
+}
+else
+{
+    Console.WriteLine("b больше, чем a");
+}
